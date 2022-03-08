6e9b213bc29c42566cdbb0f773e2349b98cb6d6c
vikramsaini26 <jeet.vikram26@gmail.com>
2022-03-08 18:50:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import React, { useState } from "react";
+const App = () => {
+  const [show, setShow] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {show ? <h1>Hello World !</h1> : <div>hi</div>}
+      <button onClick={() => setShow(true)}>Show</button>
+      <button onClick={() => setShow(false)}>Hide</button>
     </div>
   );
-}
+};
 
 export default App;
